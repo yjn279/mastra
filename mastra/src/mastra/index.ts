1147351cb6a8f08d0102ecd60d86cd58a86efea6
winnie <57951904+yjn279@users.noreply.github.com>
2026-07-11 02:09:08 +0900

    Scaffold new Mastra project (openai provider) (#2)
    * Scaffold new Mastra project (openai provider)
    
    * Remove weather agent example from scaffold
    
    ---------
    
    Co-authored-by: winnie279 <57951904+Yuji-207@users.noreply.github.com>

---
+++ mastra/src/mastra/index.ts
@@ -0,0 +1,38 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { DuckDBStore } from "@mastra/duckdb";
+import { MastraCompositeStore } from '@mastra/core/storage';
+import { Observability, MastraStorageExporter, MastraPlatformExporter, SensitiveDataFilter } from '@mastra/observability';
+
+export const mastra = new Mastra({
+  storage: new MastraCompositeStore({
+    id: 'composite-storage',
+    default: new LibSQLStore({
+      id: "mastra-storage",
+      url: "file:./mastra.db",
+    }),
+    domains: {
+      observability: await new DuckDBStore().getStore('observability'),
+    }
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'info',
+  }),
+  observability: new Observability({
+    configs: {
+      default: {
+        serviceName: 'mastra',
+        exporters: [
+          new MastraStorageExporter(), // Persists observability events to Mastra Storage
+          new MastraPlatformExporter(), // Sends observability events to Mastra Platform (if MASTRA_PLATFORM_ACCESS_TOKEN is set)
+        ],
+        spanOutputProcessors: [
+          new SensitiveDataFilter(), // Redacts sensitive data like passwords, tokens, keys
+        ],
+      },
+    },
+  }),
+});
